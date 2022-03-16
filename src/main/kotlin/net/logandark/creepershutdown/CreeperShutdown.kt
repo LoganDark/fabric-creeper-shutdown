02164fd4909a5e4b307231580b0c45440018dc66
LoganDark <logandarklock@gmail.com>
2022-03-16 07:04:43 -0700

    Initial rebrand

--- src/main/kotlin/net/logandark/creepershutdown/CreeperShutdown.kt
+++ src/main/kotlin/net/logandark/creepershutdown/CreeperShutdown.kt
@@ -1,11 +1,11 @@
-package net.logandark.barebones
+package net.logandark.creepershutdown
 
 import net.fabricmc.api.ModInitializer
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
 @Suppress("unused")
-object Barebones : ModInitializer {
+object CreeperShutdown : ModInitializer {
 	private val logger: Logger = LogManager.getLogger()
 
 	override fun onInitialize() {
